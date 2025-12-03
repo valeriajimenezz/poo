c2f864cb2a66dd4f929e3899e9cd7fd7103eecc7
valeriajimenezz <a01714375@tec.mx>
2025-12-03 12:41:48 -0600

    Add files via upload

--- Cafe.cpp
+++ Cafe.cpp
@@ -1,9 +1,14 @@
+// Archivo: Cafe.cpp
+// Descripción: Implementación de la clase Cafe
+
 #include "Cafe.h"
 
+// Constructor - Inicializar con sabor cafe
 Cafe::Cafe() : Helado() {
     sabor = "Cafe";
     intensidad = "";
 }
 
+// Getters y Setters
 string Cafe::get_intensidad() { return intensidad; }
 void Cafe::set_intensidad(string i) { intensidad = i; }
